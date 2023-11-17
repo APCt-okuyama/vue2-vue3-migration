8ab55b7cd973763d9e4b108d1a0fdd7535c56741
okuyama <t_okuyama@ap-com.co.jp>
2023-11-17 10:45:10 +0900

    (element plus) change theme color

--- sample15-element-plus-theme/src/components/HelloWorld.vue
+++ sample15-element-plus-theme/src/components/HelloWorld.vue
@@ -8,7 +8,9 @@ defineProps<{ msg: string }>()
   <p>main.tsでimportして利用できるようにする</p>
   <p>テーマカラーを変更してみる</p>
   <p>公式はこちら https://element-plus.org/en-US/guide/theming.html</p>
-  
+  <div>
+    Custom themeの変更は「SCSSをoverrideする」
+  </div>
   <h2>Element PlusのComponentを以下に並べる</h2>
   <el-row class="mb-4">
     <el-button>Default</el-button>
@@ -18,6 +20,11 @@ defineProps<{ msg: string }>()
     <el-button type="warning">Warning</el-button>
     <el-button type="danger">Danger</el-button>
   </el-row>
+
+  <div>
+    Primaryの色が変更されたらOK　
+    <p>※on demandでの利用ではないので vite.config.ts での scss.additionalDataの利用は不要</p> 
+  </div>
 
 </template>
 
--- sample15-element-plus-theme/src/main.ts
+++ sample15-element-plus-theme/src/main.ts
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
+import './styles/element/index.scss'
 import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
+//import 'element-plus/dist/index.css'
 import App from './App.vue'
 
 const app = createApp(App)
